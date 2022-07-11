bbf20820bce495b61706b09af3864508e719f5b0
19lenny <lennartlou.jung@uzh.ch>
2022-07-11 11:13:09 +0200

    fix graphes, fix naming, change query to be more efficient

--- queries/apps/4_SmartContractCallDistribution.py
+++ queries/apps/4_SmartContractCallDistribution.py
@@ -9,16 +9,17 @@ from pyspark.sql import types
 from pyspark.sql.types import StructField, StringType, LongType, DoubleType, BooleanType, StructType, IntegerType
 
 if __name__ == '__main__':
+    # config for our sparksession
     config = pyspark.SparkConf().setAll([
-        ('spark.executor.memory', '8g'),
-        ('spark.executor.cores', '3'),
-        ('spark.cores.max', '6'),
-        ('spark.driver.memory', '4g'),
+        ('spark.executor.memory', '16g'),
+        ('spark.executor.cores', '4'),
+        ('spark.cores.max', '8'),
+        ('spark.driver.memory', '2g'),
         ('spark.executor.instances', '1'),
         ('spark.dynamicAllocation.enabled', 'true'),
         ('spark.dynamicAllocation.shuffleTracking.enabled', 'true'),
         ('spark.dynamicAllocation.executorIdleTimeout', '60s'),
-        ('spark.dynamicAllocation.minExecutors', '1'),
+        ('spark.dynamicAllocation.minExecutors', '2'),
         ('spark.dynamicAllocation.maxExecutors', '2'),
         ('spark.dynamicAllocation.initialExecutors', '1'),
         ('spark.dynamicAllocation.executorAllocationRatio', '1'),
@@ -34,7 +35,7 @@ if __name__ == '__main__':
     spark = SparkSession \
         .builder \
         .config(conf=config) \
-        .appName("4_SmartContractDistributionApplication") \
+        .appName("4_smartContractDistibution") \
         .master("spark://172.23.149.212:7077") \
         .getOrCreate()
 
@@ -84,21 +85,18 @@ if __name__ == '__main__':
     # keyreg is either a node which log in to participate in the network or log off
     dfTx = dfTx.filter(dfTx.txn_type == "appl")
 
-    # account table to determine which accounts have received rewards
+    # all applications, to count the amount of applications
     dfApp = spark.read.format("mongodb") \
         .option('spark.mongodb.connection.uri', 'mongodb://172.23.149.212:27017') \
         .option('spark.mongodb.database', 'algorand') \
-        .option('spark.mongodb.collection', 'account_app') \
+        .option('spark.mongodb.collection', 'app') \
         .option('park.mongodb.read.readPreference.name', 'primaryPreferred') \
         .option('spark.mongodb.change.stream.publish.full.document.only', 'true') \
         .option("forceDeleteTempCheckpointLocation", "true") \
         .load()
 
-    # txn table to get when an address has applied to participate and when an address have determined the participation
-    # dfApp = dfApp.repartition(2)
-    # dfApp.rdd.getNumPartitions()
     # drop all unnecessary tables
-    dfApp = dfApp.select("app", "deleted", "created_at", "addr")
+    dfApp = dfApp.select("index", "created_at", "closed_at")
 
     # use case based grouping
     # problem two use cases cannot be distinct
@@ -141,9 +139,7 @@ if __name__ == '__main__':
     # clear_state, close_out, createSC, deleteSC, NoOp, opt_in, updateSC
 
     # add creation round to every group, so it can be distinguished when the group was saved
-    constant = dfApp.agg(F.max("created_at")).collect()[0][0]
-
-    dfTxGroupUseCasesGold = dfTxGroupUseCases.withColumn("CreationRound", F.lit(constant))
+    dfTxGroupUseCasesGold = dfTxGroupUseCases.withColumn("CreationRound", F.lit(newestRound))
 
     dfTxGroupUseCasesGold.write.format("mongodb") \
         .option('spark.mongodb.connection.uri', 'mongodb://172.23.149.212:27017') \
@@ -167,12 +163,13 @@ if __name__ == '__main__':
     plt.figure()
     for i in range(len(UCnames)):
         plt.bar(UCnames[i], UCvalues[i], width=0.4)
-    plt.title("smart contract use case transcation", loc='center', pad=None)
+
+    plt.title("Smart Contract Use Case Transcations", loc='center', pad=None)
     plt.savefig('/home/ubuntu/apps/figures/4_ScDistribution/SC_Use_Cases.jpg', dpi=200)
+    plt.show()
     plt.close()
 
-    # diagramm wann wie viele smart contract calls gemacht wurden
-
+    # diagramm: how much sc calls werde made
     graph = dfTx.select("round")
 
     # preparation for graph
@@ -192,7 +189,7 @@ if __name__ == '__main__':
     # histogram x-axis round when starting participating
     # how many bars in the histogram should be plotted
 
-    bin_size = 100
+    bin_size = 50
     # distribute bins log(equally) over the whole data
     mybins = np.logspace(np.log10(minSCround), np.log10(maxSCround), bin_size)
 
@@ -201,15 +198,14 @@ if __name__ == '__main__':
     plt.rcParams["figure.autolayout"] = True
     plt.xscale('log')
     plt.yscale('log')
-    plt.xlabel("blockround")
-    plt.ylabel("number of smart contract calls")
-    plt.title("smart contract call distribution", loc='center', pad=None)
+    plt.xlabel("Blockround")
+    plt.ylabel("Number of Smart Contract Calls")
+    plt.title("Smart Contract Call Distribution", loc='center', pad=None)
     plt.savefig('/home/ubuntu/apps/figures/4_ScDistribution/SC_Call_Distribution_blockround.jpg', dpi=200)
+    plt.show()
     plt.close()
 
-    # histogram jeder use case einzeln
-    # diagramm wann wie viele smart contract calls gemacht wurden
-
+    #histogram of every use case
     graphOptIn = dfTx.filter(dfTx.usecase == "opt_in").select("round")
     graphOptIn = graphOptIn.collect()
 
@@ -244,7 +240,7 @@ if __name__ == '__main__':
     graphCreate = graphCreate.collect()
 
     # convert row["data"] to only data
-    SCCreate = [row[0] for (row) in graphCreate]
+    SCcreate = [row[0] for (row) in graphCreate]
 
     graphNoOp = dfTx.filter(dfTx.usecase == "NoOp").select("round")
     graphNoOp = graphNoOp.collect()
@@ -262,8 +258,7 @@ if __name__ == '__main__':
 
     bin_size = 100
     # distribute bins log(equally) over the whole data
-    # +1 necessary??
-    mybins = np.logspace(np.log10(minH), np.log10(maxH) + 1, bin_size)
+    mybins = np.logspace(np.log10(minH), np.log10(maxH), bin_size)
 
     plt.figure()
     plt.hist(SCOptIn, bins=mybins, alpha=0.3, label="opt_in")
@@ -271,20 +266,20 @@ if __name__ == '__main__':
     plt.hist(SCClear, bins=mybins, alpha=0.3, label="clear_state")
     plt.hist(SCUpdate, bins=mybins, alpha=0.3, label="updateSC")
     plt.hist(SCDelete, bins=mybins, alpha=0.3, label="deleteSC")
-    plt.hist(SCCreate, bins=mybins, alpha=0.3, label="createSC")
+    plt.hist(SCcreate, bins=mybins, alpha=0.3, label="createSC")
     plt.hist(SCNoOp, bins=mybins, alpha=0.3, label="NoOp")
 
-    # plt.rcParams["figure.figsize"] = [7.50, 3.50]
     plt.rcParams["figure.autolayout"] = True
 
     plt.xscale('log')
     plt.yscale('log')
 
-    plt.xlabel("blockround")
-    plt.ylabel("amount")
+    plt.xlabel("Blockround")
+    plt.ylabel("Number of Calls")
     plt.legend(loc="upper right")
-    plt.title("usecase distribution", loc='center', pad=None)
+    plt.title("Use Case Distribution", loc='center', pad=None)
     plt.savefig('/home/ubuntu/apps/figures/4_ScDistribution/UseCaseOverBlockround.jpg', dpi=200)
+    plt.show()
     plt.close()
 
     spark.stop()
