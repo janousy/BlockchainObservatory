21bedc207e09aa8b911fb9fd4d9876e436af9c09
dofuerer <domenicluca.fuerer@uzh.ch>
2022-07-26 18:34:22 +0200

    update in applications configuration

--- queries/apps/4_SmartContractCallDistribution.py
+++ queries/apps/4_SmartContractCallDistribution.py
@@ -15,17 +15,9 @@ if __name__ == '__main__':
         ('spark.executor.cores', '4'),
         ('spark.cores.max', '8'),
         ('spark.driver.memory', '2g'),
-        ('spark.executor.instances', '1'),
-        ('spark.dynamicAllocation.enabled', 'true'),
-        ('spark.dynamicAllocation.shuffleTracking.enabled', 'true'),
-        ('spark.dynamicAllocation.executorIdleTimeout', '60s'),
-        ('spark.dynamicAllocation.minExecutors', '2'),
-        ('spark.dynamicAllocation.maxExecutors', '2'),
-        ('spark.dynamicAllocation.initialExecutors', '1'),
-        ('spark.dynamicAllocation.executorAllocationRatio', '1'),
+        ('spark.executor.instances', '2'),
         ('spark.worker.cleanup.enabled', 'true'),
         ('spark.worker.cleanup.interval', '60'),
-        ('spark.shuffle.service.db.enabled', 'true'),
         ('spark.worker.cleanup.appDataTtl', '60'),
         ('spark.jars.packages', 'org.mongodb.spark:mongo-spark-connector:10.0.2')
     ])
--- queries/apps/5_FT_NFT_Distribution.py
+++ queries/apps/5_FT_NFT_Distribution.py
@@ -20,16 +20,8 @@ if __name__ == '__main__':
         ('spark.cores.max', '4'),
         ('spark.driver.memory', '2g'),
         ('spark.executor.instances', '1'),
-        ('spark.dynamicAllocation.enabled', 'true'),
-        ('spark.dynamicAllocation.shuffleTracking.enabled', 'true'),
-        ('spark.dynamicAllocation.executorIdleTimeout', '60s'),
-        ('spark.dynamicAllocation.minExecutors', '1'),
-        ('spark.dynamicAllocation.maxExecutors', '2'),
-        ('spark.dynamicAllocation.initialExecutors', '1'),
-        ('spark.dynamicAllocation.executorAllocationRatio', '1'),
         ('spark.worker.cleanup.enabled', 'true'),
         ('spark.worker.cleanup.interval', '60'),
-        ('spark.shuffle.service.db.enabled', 'true'),
         ('spark.worker.cleanup.appDataTtl', '60'),
         ('spark.jars.packages', 'org.mongodb.spark:mongo-spark-connector:10.0.2')
     ])
